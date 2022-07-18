6d25c61c02acd3e3cbf020fabeb92732f80f1836
Jeremías Rombolá <106497729+jererombola@users.noreply.github.com>
2022-07-18 20:04:48 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-const port = process.env.PORT || (3000);
+const port = process.env.PORT || 3000;
 const start = () => console.log("Starting Server...");
 
 app.listen(port,start());
